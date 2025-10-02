24a662aa9507bacd3d0d0a3e18b23af43f052078
shivay-couchbase <shivay.lamba@couchbase.com>
2025-10-02 15:34:56 +0200

    webllm config

---
+++ src/utils/agent/webllm.ts
@@ -0,0 +1,8 @@
+export const GEMMA_2_9B_CONFIG = {
+    model: "https://uploads.nico.dev/mlc-llm-libs/gemma-2-9b-it_q4f16_MLC/",
+    model_id: "gemma-2-9b-it_q4f16_MLC",
+    model_lib:
+      "https://uploads.nico.dev/mlc-llm-libs/gemma-2-9b-it_q4f16_MLC/lib/gemma-2-9b-it-q4f16_1-webgpu.wasm",
+  };
+  
+  export const GEMMA_2_2B_ID = "gemma-2-2b-it-q4f16_1-MLC";
